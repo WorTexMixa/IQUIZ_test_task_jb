a8c5a5b8cafd3b30bd6e7ffed2993713eb82c86c
WorTexMixa <mihajackson2@gmail.com>
2026-09-20 11:51:31 +0300

    feat: added POP-UP triggered by a button click.

--- app/js/script.js
+++ app/js/script.js
@@ -24,3 +24,43 @@ const quizSlider = new Swiper(".quiz-types__slider", {
     },
   },
 });
+
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close");
+const modalOpenButtons = document.querySelectorAll(".button");
+
+function openModal() {
+  modal.classList.add("modal--open");
+  modal.setAttribute("aria-hidden", "false");
+
+  document.body.classList.add("modal-open");
+}
+
+function closeModal() {
+  modal.classList.remove("modal--open");
+  modal.setAttribute("aria-hidden", "true");
+
+  document.body.classList.remove("modal-open");
+}
+
+modalOpenButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    openModal();
+  });
+});
+
+modalCloseButton.addEventListener("click", closeModal);
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
